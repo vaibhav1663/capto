f3404f81e0d7dc7714e4ea3772e16723303d9eb2
Private Vaibhav <vaibhavkhating@gmail.com>
2024-12-10 12:05:03 +0530

    added a meme

--- src/components/BrowserNotSupported/BrowserNotSupported.tsx
+++ src/components/BrowserNotSupported/BrowserNotSupported.tsx
@@ -8,9 +8,11 @@ import styles from './BrowserNotSupported.module.css';
 const BrowserNotSupported = () => {
   return (
     <div className={styles.root}>
-      <Typography variant="h6">This browser is not yet supported</Typography>
+      <img src='./images/not-supported.jpg' alt='not supported' style={{ width: '100%' , maxWidth: 400 , margin: '0 auto' , borderRadius: 8 }} />
+      <Typography variant="h6">This browser/device is not yet supported</Typography>
       <Typography variant="subtitle1">
-        You can use the recorder with the following browser(s)
+        You can use the recorder with the following browser(s) preferably on a
+        desktop device
       </Typography>
       <Link
         className={styles.chromeLink}
